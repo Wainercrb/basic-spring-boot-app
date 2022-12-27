d0988eb4a4bda16efef602ab239dad3660e5289a
wainercrb <wainer.rodriguez@omni.cr>
2022-12-27 16:40:09 -0600

    feat: :sparkles: UI
    Added new UI components

--- src/main/java/com/crss/basicspringboot/BasicSpringBootApplication.java
+++ src/main/java/com/crss/basicspringboot/BasicSpringBootApplication.java
@@ -9,5 +9,4 @@ public class BasicSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BasicSpringBootApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/crss/basicspringboot/Constants.java
@@ -0,0 +1,5 @@
+package com.crss.basicspringboot;
+
+public class Constants {
+    public static final int NOT_FOUND = -1000;
+}
