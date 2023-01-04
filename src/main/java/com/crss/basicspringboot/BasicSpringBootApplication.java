f7892174f3c3bb6bd5a55954ab06f36a8289bc3f
wainercrb <wainer.rodriguez@omni.cr>
2023-01-04 15:00:50 -0600

    feat: :sparkles: Structure
    Added basic auth config

--- src/main/java/com/crss/basicspringboot/BasicSpringBootApplication.java
+++ src/main/java/com/crss/basicspringboot/BasicSpringBootApplication.java
@@ -2,11 +2,20 @@ package com.crss.basicspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.AllArgsConstructor;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class BasicSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
